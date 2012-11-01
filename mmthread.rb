6ddc7a2f43e055b3a192e5be0f8d77ccf1c11ebf
jiapei <huangw@parts.com>
2012-11-01 14:20:02 +0800

    add delayed_job_mongoid

--- mmthread.rb
+++ mmthread.rb
@@ -57,7 +57,8 @@ min_num = $*[0]
 max_num = $*[1]
 puts max_num
 
-links = Link.where( :status => 0, :lid.gte => min_num.to_i, :lid. lte => max_num.to_i)
+links = Link.where( :status => 0, :lid.gte => min_num.to_i, :lid.lte => max_num.to_i)
+puts links.count
 
 threads = []
 links.each do |link|
